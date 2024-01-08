0bfb2fb3e970e1b28dc046bbe396de3f82657751
rennanpo <rennanpontescardoso@gmail.com>
2024-01-08 15:38:55 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia s1 = new Nokia("9999", "tijolo", "seila", 4);
+Iphone i1 = new Iphone("1111", "Iphone 15", "seinao", 32);
+Nokia s2 = new Nokia("8888", "pedra", "naosei", 8);
+s1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("youtube");
+s2.InstalarAplicativo("auxilio Emergencial");
